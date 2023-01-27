787e2e62d07bbbec386b76f53aa5ca70f9aabea7
Érica Guimarães <eriquinha_guima@yahoo.com.br>
2023-01-27 19:22:44 -0300

    Exercícios para fixar finalizados

---
+++ 1_fundamentos/secao_03/dia_2/1_Para Fixar.js
@@ -0,0 +1,49 @@
+
+                    // EXERCÍCIOS PARA FIXAR 
+
+        // EXERCÍCIO 1 - ARRAYS 
+
+// Obtenha o valor “Serviços” do array menu:
+
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// let menuServices = (menu[1]);
+
+// console.log(menuServices);
+
+
+        // EXERCÍCIO 2 - ARRAYS
+
+// Procure o índice do valor “Portfólio” do array menu:
+
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// let indexOfPortfolio = (menu[2]);
+
+// console.log(indexOfPortfolio);
+
+        // EXERCÍCIO 3 - ARRAY 
+
+// Adicione o valor “Contato” no final do array menu:
+// De olho na dica 👀: Use o método .push()
+
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// menu.push("Contato");
+// console.log(menu);
+
+        // EXERCÍCIO 4 - FOR 
+
+// Utilize o for para imprimir os elementos da lista groceryList com o console.log():
+
+// let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+// for (let list = 0; list < groceryList.length; list += 1){
+//     console.log(groceryList[list])
+// }
+
+//         EXERCÍCIO 5 - FOR/OF
+
+// Utilize o for/of para imprimir os elementos da lista names com o console.log():
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+for (let lista of names){
+    console.log(lista);
+}

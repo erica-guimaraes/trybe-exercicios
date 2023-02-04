eeb92b40cf59f9e6f9877931457ebf3843d22c72
Érica Guimarães <eriquinha_guima@yahoo.com.br>
2023-02-04 18:15:38 -0300

    Ex_Aprofunde Conhecimento sobre DOM finalizado

--- 1_fundamentos/secao_04/dia_2/Ex_Aprofunde Seu Conhecimento/script.js
+++ 1_fundamentos/secao_04/dia_2/Ex_Aprofunde Seu Conhecimento/script.js
@@ -2,13 +2,52 @@
 // Para dar início a página, você deve criar algumas tags:
 
 // Adicione a tag h1 com o texto TrybeTrip - Agência de Viagens como filho da tag body;
+
+const body = document.getElementById("body");
+const elementH1 = document.creatElement("h1");
+body.appendChild(elementH1);
+
 // Adicione a tag main com a classe main-content como filho da tag body;
+
+const mainContent = document.creatElement("main");
+mainContent.className = "main-content";
+body.appendChild(mainContent);
+
 // Adicione a tag section com a classe center-content como filho da tag main criada no passo 2;
+
+const centerContent = document.creatElement("section");
+centerContent.className = "center-content";
+mainContent.appendChild(centerContent);
+
 // Adicione a tag p como filho do section criado no passo 3 e coloque algum texto;
+
+const elementP = document.creatElement("p");
+centerContent.appendChild(elementP);
+elementP.innerText = "Aprofundando o Conhecimento com o DOM"
+
 // Adicione a tag section com a classe left-content como filho da tag main criada no passo 2;
+
+const leftContent = document.creatElement("section");
+leftContent.className = "left-content";
+mainContent.appendChild(leftContent);
+
 // Adicione a tag section com a classe right-content como filho da tag main criada no passo 2;
+
+const rightContent = document.creatElement("section");
+rightContent.className = "right-content";
+mainContent.appendChild(rightContent);
+
 // Adicione uma imagem com src configurado para o valor https://picsum.photos/200 e classe small-image. Esse elemento deve ser filho do section criado no passo 5;
-// Adicione uma lista não ordenada com os valores de 1 a 10 por extenso, ou seja, um, dois, três, … como valores da lista. Essa lista deve ser filha do section criado no passo 6;
+
+const imagem = document.createElement("img");
+imagem.className = "small-image";
+imagem.src = https://picsum.photos/200;
+leftContent.appendChild(imagem);
+
+// Adicione uma lista não ordenada com os valores de 1 a 10 por extenso, ou seja, um, dois, três, … como valores da lista. 
+// Essa lista deve ser filha do section criado no passo 6;
+
+
 // Adicione 3 tags h3, todas sendo filhas do main criado no passo 2.
 // Após criar as tags anteriores, você mostrou para a sua liderança como estava a estrutura inicial da página. Sua liderança então pediu para que você fizesse algumas alterações:
 

cd7d7ca9ddc58e55fc609ff184e2e7b04902bf17
Érica Guimarães <eriquinha_guima@yahoo.com.br>
2023-02-04 15:23:01 -0300

    Ex Buscando Elementos

---
+++ 1_fundamentos/secao_04/dia_2/script.js
@@ -0,0 +1,17 @@
+// 🚀  Adicione o código abaixo a uma página HTML e adicione uma tag script. Você deverá fazer tudo usando somente JavaScript.
+
+// Acesse o elemento elementoOndeVoceEsta.
+// Acesse pai a partir de elementoOndeVoceEsta e adicione uma color a ele.
+// Acesse o primeiroFilhoDoFilho e adicione um texto a ele. Você se lembra dos vídeos da aula anterior, como fazer isso?
+// Acesse o primeiroFilho a partir de pai.
+// Agora acesse o primeiroFilho a partir de elementoOndeVoceEsta.
+// Agora acesse o texto Atenção! a partir de elementoOndeVoceEsta.
+// Agora acesse o terceiroFilho a partir de elementoOndeVoceEsta.
+// Agora acesse o terceiroFilho a partir de pai. -->
+
+const elementoOndeVoceEsta = document.getElementById("elementoOndeVoceEsta");
+
+const pai = elementoOndeVoceEsta.parentNode;
+pai.style.color = "red";
+
+const primeiroFilhoDoFilho = 

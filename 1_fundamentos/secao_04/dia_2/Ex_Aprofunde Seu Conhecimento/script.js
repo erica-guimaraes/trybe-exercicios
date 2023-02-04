964568a9a403042e2d580cddf182a72677aaed32
Érica Guimarães <eriquinha_guima@yahoo.com.br>
2023-02-04 18:21:53 -0300

    Conferindo

--- 1_fundamentos/secao_04/dia_2/Ex_Aprofunde Seu Conhecimento/script.js
+++ 1_fundamentos/secao_04/dia_2/Ex_Aprofunde Seu Conhecimento/script.js
@@ -47,15 +47,55 @@ leftContent.appendChild(imagem);
 // Adicione uma lista não ordenada com os valores de 1 a 10 por extenso, ou seja, um, dois, três, … como valores da lista. 
 // Essa lista deve ser filha do section criado no passo 6;
 
+const listaNaoOrdenada = document.createElement("ul");
+rightContent.appendChild(listaNaoOrdenada); 
+const numbers = ["um", "dois", "três", "quatro", "cinco", "seis", "sete", "oito", "nove", "dez"];
+    for (let index in numbers) {
+        const elementLi = document.createElement("li");
+        elementLi.innerHTML = numbers[index];
+        listaNaoOrdenada.appendChild(elementLi);
+    }
 
 // Adicione 3 tags h3, todas sendo filhas do main criado no passo 2.
-// Após criar as tags anteriores, você mostrou para a sua liderança como estava a estrutura inicial da página. Sua liderança então pediu para que você fizesse algumas alterações:
+
+for (let index = 1; index <= 3; index += 1) {
+    const elementH3 = document.createElement('h3');
+    elementH3 = index;
+    mainContent.appendChild(elementH3);
+}
+// ???????????????????????????????????
+
+// Após criar as tags anteriores, você mostrou para a sua liderança como estava a estrutura inicial da página. 
+// Sua liderança então pediu para que você fizesse algumas alterações:
 
 // Adicione a classe title na tag h1 criada;
+
+const title = document.createElement("h1");
+title.className = "title";
+
 // Adicione a classe description nas 3 tags h3 criadas;
+
+const description = document.createElement("h3");
+description.className = "description";
+mainContent.appendChild("description");
+// ???????????????????????????????????????
+
 // Remova a section criada no passo 5 (aquele que possui a classe left-content). Utilize a função .removeChild();
+
+const sectionLeftContent = document.getElementsByClassName("left-content");
+mainContent.removeChild("sectionLeftContent");
+
 // Centralize a section criada no passo 6 (aquele que possui a classe right-content).
+
+const sectionRightContent = document.getElementsByClassName("right-content")[0];
+sectionRightContent.style.marginRight="auto";
+
 // De olho na dica 👀:: Para centralizar a section, basta configurar o margin-right: auto da section;
 
 // Troque a cor de fundo do elemento pai da section criada no passo 3 (aquela que possui a classe center-content) para a cor verde;
+
+mainContent.style.blackgroudColor = "grren";
+
 // Remova os dois últimos elementos (nove e dez) da lista criada no passo 8.
+
+// ??????????????????????

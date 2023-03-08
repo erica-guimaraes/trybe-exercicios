2a15702e65f049f04f5d0aa4e0d1deefc12c5ffd
Érica Guimarães <eriquinha_guima@yahoo.com.br>
2023-03-08 18:44:23 -0300

    Parte II quase finalizada, dia 7.1

--- 1_fundamentos/secao_07/dia_1/Ex-Agora a prática/script.js
+++ 1_fundamentos/secao_07/dia_1/Ex-Agora a prática/script.js
@@ -80,4 +80,139 @@ const countPoints = (rightAnswers, studentAnswers, action) => {
 
 console.log(countPoints(RIGHT_ANSWERS, STUDENT_ANSWERS, compareAnswers));
 
+// 🚀 Parte II - Organizando uma biblioteca
 
+// Estes exercícios praticam os conceitos de Higher Order Functions associados a outros temas de fundamentos já vistos, como arrow functions, template literals e objetos. Essa mistura de conceitos é muito importante para o seu aprendizado, então use tudo o que sabe para resolver os exercícios!
+
+// Utilize o seguinte código como template para realizar os exercícios:
+
+const books = [
+    {
+      id: 1,
+      name: 'As Crônicas de Gelo e Fogo',
+      genre: 'Fantasia',
+      author: {
+        name: 'George R. R. Martin',
+        birthYear: 1948,
+      },
+      releaseYear: 1991,
+    },
+    {
+      id: 2,
+      name: 'O Senhor dos Anéis',
+      genre: 'Fantasia',
+      author: {
+        name: 'J. R. R. Tolkien',
+        birthYear: 1892,
+      },
+      releaseYear: 1954,
+    },
+    {
+      id: 3,
+      name: 'Fundação',
+      genre: 'Ficção Científica',
+      author: {
+        name: 'Isaac Asimov',
+        birthYear: 1920,
+      },
+      releaseYear: 1951,
+    },
+    {
+      id: 4,
+      name: 'Duna',
+      genre: 'Ficção Científica',
+      author: {
+        name: 'Frank Herbert',
+        birthYear: 1920,
+      },
+      releaseYear: 1965,
+    },
+    {
+      id: 5,
+      name: 'A Coisa',
+      genre: 'Terror',
+      author: {
+        name: 'Stephen King',
+        birthYear: 1947,
+      },
+      releaseYear: 1986,
+    },
+    {
+      id: 6,
+      name: 'O Chamado de Cthulhu',
+      genre: 'Terror',
+      author: {
+        name: 'H. P. Lovecraft',
+        birthYear: 1890,
+      },
+      releaseYear: 1928,
+    },
+  ];
+  
+// Encontre o nome da primeira pessoa autora do livro nascida no ano de 1947.
+// De olho na dica 👀: use a função find.
+
+// const expectedResult = 'Stephen King';
+
+const authorBornIn1947 = () => {
+  return books.find((book) => book.author.birthYear === 1947).author.name;
+};
+console.log(authorBornIn1947());
+
+// Retorne o nome do livro com menor número de caracteres (menor nome).
+// De olho na dica 👀: use a função forEach.
+
+// const expectedResult = 'Duna';
+
+const smallerName = () => {
+  let nameBook;
+  books.forEach((book) => {
+    if (!nameBook || book.name.length < nameBook.length){
+      nameBook = book.name
+    }
+  })
+  return nameBook;
+};
+ console.log(smallerName());
+
+//  Encontre o primeiro livro cujo nome possua 26 caracteres.
+
+// const expectedResult = {
+//   author: {
+//     birthYear: 1948,
+//     name: 'George R. R. Martin',
+//   },
+//   genre: 'Fantasia',
+//   id: 1,
+//   name: 'As Crônicas de Gelo e Fogo',
+//   releaseYear: 1991,
+// };
+
+const getNamedBook = () => books.find((book) => book.name.length === 26);
+
+console.log(getNamedBook());
+
+// Faça uma função que retorne true se todas as pessoas autoras tiverem nascido no século XX, ou false, caso contrário.
+
+// const expectedResult = false;
+
+const everyoneWasBornOnSecXX = () => books.every((year) => year.author.birthYear >= 1901 && year.author.birthYear <= 2000);
+
+console.log(everyoneWasBornOnSecXX());
+
+// Faça uma função que retorne true, se algum livro foi lançado na década de 80, e false, caso contrário.
+
+// const expectedResult = true;
+
+const someBookWasReleaseOnThe80s = () => books.some((book) => book.releaseYear >= 1980 && book.releaseYear <= 1989);
+
+console.log(someBookWasReleaseOnThe80s());
+
+// Faça uma função que retorne true, caso nenhuma pessoa autora tenha nascido no mesmo ano, e false, caso contrário.
+
+const expectedResult = false;
+
+const authorUnique = () => {
+  return books.every((autor) =>
+   autor.author.birthYear)
+};

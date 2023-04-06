4031b2192700161fdb2cddb642995725569cd4de
Érica Guimarães <eriquinha_guima@yahoo.com.br>
2023-04-06 19:02:22 -0300

    Exercício de Fixação Buttons - 2 parte

--- 2_front-end/secao_03/dia_1/Ex_Fixacao/fancy-buttons/src/App.js
+++ 2_front-end/secao_03/dia_1/Ex_Fixacao/fancy-buttons/src/App.js
@@ -1,25 +1,31 @@
 import React from 'react';
 import './App.css';
 
-const handleClickOne = () => {
-  console.log('Clicou no botão 1!')
-};
-
-const handleClickTwo = () => {
-  console.log('Clicou no botão 2!')
-};
-
-const handleClickThree = () => {
-  console.log('Clicou no botão 3!')
-};
-
 class App extends React.Component {
+  constructor() {
+    super();
+    this.handleClickOne = this.handleClickOne.bind(this);
+    this.handleClickTwo = this.handleClickTwo.bind(this);
+    this.handleClickThree = this.handleClickThree.bind(this);
+  }
+
+  handleClickOne = () => {
+    console.log('Clicou no botão 1!', this)
+  };
+  
+  handleClickTwo = () => {
+    console.log('Clicou no botão 2!', this)
+  };
+  
+  handleClickThree = () => {
+    console.log('Clicou no botão 3!', this)
+  };
   render() {
     return (
       <div>
-        <button onClick={ handleClickOne }>Botão 1 </button>
-        <button onClick={ handleClickTwo }>Botão 2 </button>
-        <button onClick={ handleClickThree }>Botão 3 </button>
+        <button onClick={this.handleClickOne}>Botão 1 </button>
+        <button onClick={this.handleClickTwo}>Botão 2 </button>
+        <button onClick={this.handleClickThree}>Botão 3 </button>
       </div>
       
     );
